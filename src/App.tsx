c8c31a2a13b27e303be2756044708e7e9c974680
MoinMarcell <marcell.dechant@protonmail.com>
2023-02-18 12:37:37 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import TodoApp from "./component/TodoApp/TodoApp";
+import {Todo} from "./model/Todo";
+import axios from "axios";
+import TodoGallery from "./component/TodoGallery/TodoGallery";
+import AddTodo from "./component/AddTodo/AddTodo";
 
 function App() {
-  return (
-    <div className="App">
-      <TodoApp />
-    </div>
-  );
+
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+
+    function fetchTodos() {
+        axios.get("/api/todo")
+            .then((response) => {
+                setTodos(response.data);
+            })
+            .catch((error) => {
+                console.error("Something went wrong :(" + error);
+            });
+    }
+
+    function addToDo(todoToAdd: Todo) {
+        axios.post("/api/todo", todoToAdd)
+            .then(() => {
+                fetchTodos();
+            })
+            .catch((error) => {
+                console.error("Something went wrong :(" + error)
+            });
+    }
+
+    function deleteTodo(id: string) {
+        axios.delete("/api/todo/" + id)
+            .then(() => {
+                fetchTodos();
+            })
+            .catch((error) => {
+                console.error("Something went wrong :(" + error);
+            });
+    }
+
+    function updateTodo(id: string, todo: Todo) {
+        axios.put("/api/todo/" + id + "/update", todo)
+            .then(() => {
+                fetchTodos();
+            })
+            .catch((error) => {
+                console.error("Something went wrong :(" + error);
+            })
+    }
+
+    return (
+        <div className="App">
+            <AddTodo addTodo={addToDo}/>
+            <TodoGallery updateTodo={updateTodo} deleteTodo={deleteTodo} todos={todos}/>
+        </div>
+    );
 }
 
 export default App;
--- src/component/TodoGallery/TodoGallery.tsx
+++ src/component/TodoGallery/TodoGallery.tsx
@@ -4,20 +4,44 @@ import "./TodoGallery.css";
 
 type TodoGalleryProps = {
     todos: Todo[],
-    filter: "OPEN" | "IN_PROGRESS" | "DONE",
+    deleteTodo(id: string): void,
+    updateTodo(id: string, todo: Todo): void,
 }
 
 export default function TodoGallery(props: TodoGalleryProps){
 
-    const todoCard = props.todos.filter((todo) => todo.status === props.filter).map((todo) => {
+    const openTodos = props.todos.filter((todo) => todo.status === "OPEN").map((todo) => {
         return(
-            <TodoCard todo={todo} key={todo.id + " " + todo.status + " " + todo.description} />
+            <TodoCard updateTodo={props.updateTodo} deleteTodo={props.deleteTodo} todo={todo} key={todo.id + " " + todo.status + " " + todo.description} />
         );
-    });
+    }).reverse();
+
+    const progressTodos = props.todos.filter((todo) => todo.status === "IN_PROGRESS").map((todo) => {
+        return(
+            <TodoCard updateTodo={props.updateTodo} deleteTodo={props.deleteTodo} todo={todo} key={todo.id + " " + todo.status + " " + todo.description} />
+        );
+    }).reverse();
+
+    const doneTodos = props.todos.filter((todo) => todo.status === "DONE").map((todo) => {
+        return(
+            <TodoCard updateTodo={props.updateTodo} deleteTodo={props.deleteTodo} todo={todo} key={todo.id + " " + todo.status + " " + todo.description} />
+        );
+    }).reverse();
 
     return(
         <div className={"todo-gallery"}>
-            {todoCard}
+            <div className={"todo-gallery-status"}>
+                <h2>OPEN</h2>
+                {openTodos}
+            </div>
+            <div className={"todo-gallery-status"}>
+                <h2>PROGRESS</h2>
+                {progressTodos}
+            </div>
+            <div className={"todo-gallery-status"}>
+                <h2>DONE</h2>
+                {doneTodos}
+            </div>
         </div>
     );
 
